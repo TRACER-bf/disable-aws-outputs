80045667c3a9de23e968d47524a940f174a615a6
iwanpark <iwanpark.abc@gmail.com>
2024-11-22 09:50:58 +0900

    support custom.retainedOutputs

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const ref = {}
-
 class DisableOutputs {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.service = serverless.service;
+    this.hooks = {
+      "before:package:finalize": this.disableOutputs.bind(this),
+    };
+  }
 
-    constructor(serverless, options) {
-        const self = this
-        ref.self = self
-        this.serverless = serverless
-        this.service = serverless.service
-        this.hooks = {
-            'before:package:finalize': this.disableOutputs.bind(this)
-        }
+  disableOutputs() {
+    const cfnTemplate =
+      this.serverless.service.provider.compiledCloudFormationTemplate;
+    if (!cfnTemplate["Outputs"]) return;
 
-    }
+    const retainedOutputs =
+      this.serverless.service.custom?.retainedOutputs || [];
 
-    disableOutputs() {
-        const cfnTemplate = this.serverless.service.provider.compiledCloudFormationTemplate
-        cfnTemplate["Outputs"] = {}
-    }
-
-
+    Object.keys(cfnTemplate["Outputs"]).forEach((outputKey) => {
+      if (retainedOutputs.includes(outputKey)) return;
+      delete cfnTemplate["Outputs"][outputKey];
+    });
+  }
 }
 
-module.exports = DisableOutputs
+module.exports = DisableOutputs;
